c890ee6d8f1681b137bd12bf25999089a528266b
Muhammad Fikri Fernanda Yusuf <68997783+mfikrif11@users.noreply.github.com>
2022-10-13 11:34:59 +0700

    fix navbar (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { Box, Text } from "@chakra-ui/react"
+import { Routes, Route, Link } from "react-router-dom"
+import LoginPage from "./pages/login"
+import RegisterPage from "./pages/register"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
 const App = () => {
     return (
-        <Box>
-            <Text>HELLO</Text>
-        </Box>
+        <>
+            <Box>
+                {/* #43615f hero color*/}
+                <Navbar />
+            </Box>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
     )
 }
 
