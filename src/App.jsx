0ff5865285dc06f76645282f963c7feb32fb30f0
Wibi Hanif Wibowo <wibihanifwibowo@gmail.com>
2022-10-13 11:17:27 +0700

    register and login page
    masih kurang bagus

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react";
+import { Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 const App = () => {
-    return (
-        <Box>
-            <Text>HELLO</Text>
-        </Box>
-    )
-}
+  return (
+    <>
+      <Box>
+        <Text>NAVBAR</Text>
+      </Box>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:2000",
+});
+
+axiosInstance.interceptors.request.use((req) => {
+  const auth_token = localStorage.getItem("auth_token");
+
+  if (auth_token) {
+    req.headers.authorization = `Bearer ${auth_token}`;
+  }
+  // ngegetnya dari headers authorization biar secure
+
+  return req;
+});
+
+// axiosInstance.interceptors.response.use(
+//   (resSuccess) => {
+//     return resSuccess;
+//   },
+//   (resError) => {
+//     // console.log(resError);
+//     if (resError.response.status === 401) {
+//       console.log("LOGOUT USER");
+//       localStorage.removeItem("auth_token");
+//       store.dispatch(logout());
+//       // ini gk pake useDispatch karena bukan format jsx
+//     }
+
+//     return Promise.reject(resError);
+//   }
+// );
+
+export { axiosInstance };
