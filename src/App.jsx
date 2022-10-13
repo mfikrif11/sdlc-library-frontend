b7ff032b74cd75484157d4c00a23787aa8c3ed43
joel374 <88383646+joel374@users.noreply.github.com>
2022-10-13 11:12:16 +0700

    Epis-1
    Navbar blm ada cart
    responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { Box, Text } from "@chakra-ui/react"
+import { Route, Router, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
 const App = () => {
-    return (
-        <Box>
-            <Text>HELLO</Text>
-        </Box>
-    )
+  return (
+    <>
+      <Box>
+        {/* #43615f hero color*/}
+        <Navbar />
+      </Box>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" />
+        <Route path="/register" />
+      </Routes>
+    </>
+  )
 }
 
 export default App
