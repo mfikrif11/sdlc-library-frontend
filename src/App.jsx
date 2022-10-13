15a6f7ab74aa3ff1e7c0ff59b98a10c1dae549b3
Muhammad Fikri Fernanda Yusuf <fernanda.fikri11@gmail.com>
2022-10-13 11:33:31 +0700

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { Box, Text } from "@chakra-ui/react";
-import { Routes, Route, Link } from "react-router-dom";
-import LoginPage from "./pages/login";
-import RegisterPage from "./pages/register";
+import { Box, Text } from "@chakra-ui/react"
+import { Routes, Route, Link } from "react-router-dom"
+import LoginPage from "./pages/login"
+import RegisterPage from "./pages/register"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 
 const App = () => {
-  return (
-    <>
-      <Box>
- {/* #43615f hero color*/}
-           <Navbar />
-      </Box>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-           <Route path="/" element={<Home />} />
-      </Routes>
-    </>
-  );
-};
+    return (
+        <>
+            <Box>
+                {/* #43615f hero color*/}
+                <Navbar />
+            </Box>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    )
+}
 
-export default App;
+export default App
