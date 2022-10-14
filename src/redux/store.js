46bcb3f984714a12b9c755375b9af02195fdb568
Muhammad Fikri Fernanda Yusuf <fernanda.fikri11@gmail.com>
2022-10-14 10:06:26 +0700

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
+<<<<<<< Updated upstream
 import bookSlice from "./features/bookSlice"
 
 export const store = configureStore({ reducer: { book: bookSlice } })
+=======
+import authSlice from "./features/authSlice"
+import bookSlice from "./features/bookSlice"
+
+export const store = configureStore({
+    reducer: {
+        auth: authSlice,
+        book: bookSlice,
+    },
+})
+>>>>>>> Stashed changes
