b2d24431875b73e40d533631cee9b7d2f392ee27
joel374 <88383646+joel374@users.noreply.github.com>
2022-10-14 10:08:11 +0700

    edit home
    ya gitulah

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
+import { ChevronDownIcon } from "@chakra-ui/icons"
 import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
   Box,
   Button,
   color,
@@ -6,21 +10,63 @@ import {
   Flex,
   Grid,
   GridItem,
+  HStack,
   Image,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Select,
   Text,
 } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
 import { axiosInstance } from "../api"
 import Book from "../components/Book"
 
 const Home = () => {
   const [books, setBooks] = useState([])
+  const [totalCount, setTotalCount] = useState(0)
+  const [page, setPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(1)
+  const [sort, setSort] = useState("")
+
+  const [inputFilter, setInputFilter] = useState("")
+  const [currentFilter, setCurrentFilter] = useState("")
+  // const [minPage, setMinPage] = useState(1)
+  // const [fruits, setFruits] = useState([...fetchBooks()])
+  const filterBtnHandler = () => {
+    setCurrentFilter(inputFilter)
+  }
+
+  // const renderFruits = () => {
+  //   return fruits.map((val) => {
+  //     if (val.toLowerCase().includes(currentFilter.toLowerCase())) {
+  //       return <li>{val}</li>
+  //     }
+  //   })
+  // }
 
   const fetchBooks = async () => {
+    const maxItemsPerPage = 12
+
     try {
-      const response = await axiosInstance.get("/books")
-      setBooks(response.data.data)
+      const response = await axiosInstance.get(`books?_sortBy=${setSort}`, {
+        params: {
+          _page: page,
+          _limit: maxItemsPerPage,
+        },
+      })
+
+      setTotalCount(response.data.dataCount)
+      setMaxPage(Math.ceil(response.data.dataCount / maxItemsPerPage))
+
+      if (page === 1) {
+        setBooks(response.data.data)
+        // setMinPage(Math.ceil(response.data.dataCount / maxItemsPerPage))
+      } else {
+        setBooks(response.data.data)
+      }
     } catch (error) {
       console.log(error)
     }
@@ -40,10 +86,19 @@ const Home = () => {
       )
     })
   }
+  const sortBook = () => {
+    setSort(sort + "")
+  }
 
+  const seeMoreBtnHandler = () => {
+    setPage(page + 1)
+  }
+  const previouspage = () => {
+    setPage(page - 1)
+  }
   useEffect(() => {
     fetchBooks()
-  }, [])
+  }, [page])
 
   return (
     <>
@@ -122,17 +177,89 @@ const Home = () => {
         <Grid templateColumns="repeat(3, 1fr)" gap={6}>
           <GridItem></GridItem>
           <GridItem>
-            <Text
-              textAlign={"center"}
-              fontSize="48px"
-              fontWeight={"bold"}
-              fontFamily="sans-serif"
-              justifyContent={"center"}
-              mt="60px"
-              id="ourbooks"
-            >
-              Our Books
-            </Text>
+            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+              <GridItem>
+                <Box display={"flex"} mt="80px">
+                  <Text
+                    fontSize={"18px"}
+                    my="auto"
+                    mr={"20px"}
+                    fontWeight="semibold"
+                  >
+                    Filter
+                  </Text>
+                  <Menu>
+                    <MenuButton as={Button}>Genre</MenuButton>
+                    <MenuList>
+                      <MenuItem>Action</MenuItem>
+                      <MenuItem>Adventure</MenuItem>
+                      <MenuItem>Biography</MenuItem>
+                      <MenuItem>Comedy</MenuItem>
+                      <MenuItem>Coming Of Age</MenuItem>
+                      <MenuItem>Education</MenuItem>
+                      <MenuItem>Fantasy</MenuItem>
+                      <MenuItem>Fiction</MenuItem>
+                      <MenuItem>Historical</MenuItem>
+                      <MenuItem>Religion</MenuItem>
+                      <MenuItem>Romance</MenuItem>
+                      <MenuItem>Sci-fi</MenuItem>
+                      <MenuItem>Self-help book</MenuItem>
+                    </MenuList>
+                  </Menu>
+                </Box>
+              </GridItem>
+              <GridItem>
+                <Text
+                  textAlign={"center"}
+                  fontSize="48px"
+                  fontWeight={"bold"}
+                  fontFamily="sans-serif"
+                  justifyContent={"center"}
+                  mt="60px"
+                  id="ourbooks"
+                >
+                  Our Books
+                </Text>
+              </GridItem>
+              <GridItem display={"flex"}>
+                <Box display={"flex"} mt="80px" justifyContent={"left"}>
+                  <Input
+                    placeholder="Search"
+                    onChange={(event) => {
+                      setInputFilter(event.target.value)
+                    }}
+                  />
+                  <Button
+                    ml={"1"}
+                    bgColor="#43615f"
+                    color={"white"}
+                    onClick={filterBtnHandler}
+                  >
+                    Search
+                  </Button>
+                </Box>
+                <Box display={"flex"} mt="80px" justifyContent={"right"}>
+                  <Text
+                    fontSize={"18px"}
+                    my="auto"
+                    mr={"20px"}
+                    ml={"20px"}
+                    fontWeight="semibold"
+                  >
+                    Sort
+                  </Text>
+
+                  <Select width={"120px"}>
+                    <option value="title" onClick={sortBook}>
+                      A - Z
+                    </option>
+                    <option value="desc">Z - A</option>
+                    <option value="latest">Latest</option>
+                    <option value="old">Old</option>
+                  </Select>
+                </Box>
+              </GridItem>
+            </Grid>
 
             <Grid
               templateColumns={{
@@ -144,6 +271,39 @@ const Home = () => {
               mt="15px"
             >
               {renderBooks()}
+            </Grid>
+            <Grid templateColumns="repeat(3, 1fr)" mt="15px">
+              <GridItem />
+              <GridItem />
+              <GridItem>
+                <HStack justifyContent={"end"} gap="2px">
+                  {page === 1 ? null : (
+                    <Button
+                      bgColor={"#43615f"}
+                      onClick={previouspage}
+                      color="white"
+                    >
+                      Prev
+                    </Button>
+                  )}
+                  {!books.length ? (
+                    <Alert status="warning">
+                      <AlertIcon />
+                      <AlertTitle>No posts found</AlertTitle>
+                    </Alert>
+                  ) : null}
+                  {page >= maxPage ? null : (
+                    <Button
+                      bgColor={"#43615f"}
+                      color="white"
+                      onClick={seeMoreBtnHandler}
+                      // isDisabled={books.length <= totalCount}
+                    >
+                      Next
+                    </Button>
+                  )}
+                </HStack>
+              </GridItem>
             </Grid>
           </GridItem>
           <GridItem></GridItem>
